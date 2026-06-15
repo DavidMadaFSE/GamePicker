1949f184461bcc2fa21af1658319c8da1b62ee3f
David <dmada@asu.edu>
2026-06-15 05:34:38 -0700

    Use H2 for backend CI tests

--- backend/src/test/java/com/david/nextplay/NextplayApplicationTests.java
+++ backend/src/test/java/com/david/nextplay/NextplayApplicationTests.java
@@ -2,8 +2,10 @@ package com.david.nextplay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NextplayApplicationTests {
 
 	@Test
